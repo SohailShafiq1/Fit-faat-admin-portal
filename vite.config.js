d96c46652192d768191f093c625f56f92104523f
SohailShafiq1 <Sohail.shafiq002@gmail.com>
2026-02-15 01:19:30 +0500

    vite.js

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/',           // 🔥 important for Netlify
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5001',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api')
-      }
-    }
-  }
 });
